f0f055ccf50eda2ed9b8c90f47c02cb3920cae5f
Atish Kumar <atishkumar31518@gmail.com>
2024-10-22 21:36:06 +0530

    commit-for-Models-pushed-git

--- catalyst_app/models.py
+++ catalyst_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255)
+    year_founded = models.IntegerField()
+    industry = models.CharField(max_length=255)
+    size_range = models.CharField(max_length=255)
+    locality = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    linkedin_url = models.CharField(max_length=255)
+    current_employee_estimate = models.IntegerField()
+    total_employee_estimate = models.IntegerField()
+
+
+    # Add fields matching your CSV data
